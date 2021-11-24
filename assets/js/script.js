4551550ec2567aa3b14ee14e95813b907ee21aa3
paperpatch <patchen21@gmail.com>
2021-11-24 15:51:51 -0500

    changed fade in/out function so that it loads on page instead of relying on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,44 +10,21 @@ window.onscroll = function () {
   prevScrollpos = currentScrollpos;
 }
 
-// Fade in Elements Section
+// Fade In/Out Elements Section
 
-let numberOfScans = 1
-// console.log(window.innerHeight);
-
-function isVisible(element) {
-  let elementBox = element.getBoundingClientRect();
-  let distanceFromTop = -200;
-
-  if (elementBox.top - window.innerHeight < distanceFromTop) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function scanDocument() {
-  let sectionList = document.querySelectorAll('.hidden');
-  sectionList.forEach(function (section) {
-    if (isVisible(section)) {
-      section.classList.remove('hidden');
-    };
-  });
-
-  // console.log(numberOfScans);
-  // numberOfScans++;
-}
-// Because scroll events might be too much for the site to handle, a throttle function is introduced.
-// Use lodash library to replace function for handling throttle better.
-
-// function throttle(fn, wait) {
-//   let time = Date.now();
-//   return function() {
-//     if ((time + wait - Date.now()) < 0 ) {
-//       fn();
-//       time = date.now();
-//     }
-//   }
-// }
-
-document.addEventListener("scroll", _.throttle(scanDocument, 500));
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top //+ $(this).outerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
