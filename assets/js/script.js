4778d3420f6a295e05f65a0b59846caa5048ccab
paperpatch <patchen21@gmail.com>
2021-11-27 00:37:02 -0500

    changed height/width to fix fading/in/out on mobile responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,13 @@ window.onscroll = function () {
 
 $(window).on("load",function() {
   $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    let windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade").each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top //+ $(this).outerHeight();
+      let objectTop = $(this).offset().top //+ $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      if (objectTop < windowBottom) { //object comes into view (scrolling down)
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
       } else { //object goes out of view (scrolling up)
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
