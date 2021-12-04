13717e4cd18934d1f0f3e8731da30b8052d8c8be
paperpatch <patchen21@gmail.com>
2021-12-04 11:23:48 -0500

    added bootstrap modal for resume btn and changed .fade class to .fade-in to avoid conflicts with bootstrap fade class

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,16 +15,16 @@ window.onscroll = function () {
 $(window).on("load",function() {
   $(window).scroll(function() {
     let windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade").each(function() {
+    $(".fade-in").each(function() {
       /* Check the location of each desired element */
       let objectTop = $(this).offset().top //+ $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
       if (objectTop < windowBottom) { //object comes into view (scrolling down)
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-      }
+      } // else { //object goes out of view (scrolling up)
+        // if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      // }
     });
   }).scroll(); //invoke scroll-handler on page-load
 });
