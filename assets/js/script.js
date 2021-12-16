000c434886453d9244dbfebbbdaca98b7430bddb
paperpatch <patchen21@gmail.com>
2021-12-16 12:58:34 -0500

    adjusted styling and height for better mobile responsiveness. Reconfigured arrow bootstrap layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,17 @@ window.onscroll = function () {
 
 // Fade In Elements Section
 
-$(window).scroll(function() {
-  $('.fade-in').each(function() {
-    let top_of_element = $(this).offset().top;
-    let bottom_of_element = $(this).offset().top + $(this).outerHeight();
-    let bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-    let top_of_screen = $(window).scrollTop();
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    $('.fade-in').each(function() {
+      let top_of_element = $(this).offset().top;
+      let bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      let bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+      let top_of_screen = $(window).scrollTop();
 
-    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
-      $(this).addClass('is-visible');
-    }
+      if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
+        $(this).addClass('is-visible');
+      }
+    });
   });
 });
